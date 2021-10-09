81bd38c439022c5c4b81b3d2cb482175db0db8f3
Dkriss <89033170+Dkriss@users.noreply.github.com>
2021-10-09 15:37:39 +0700

    Update Nyoba.c

--- Nyoba.c
+++ Nyoba.c
@@ -1,43 +1,22 @@
-// CATATAN 
-    /* Iki aku lak njajal nggawe rata-rata dam, lah kadang aku jek kebingungan lek sing nggawe cara biasa karo sing cara loop+array, iki tak duduhi garapanku. Gurung mari garapanku,
-        sepurane lek jek cekeremes uelek wkwkwkwk. Tulung duduhno sing nggawe cara manual biasa karo sing loop+array. Sing garapanku ojo dihapus, copas trus dekek ning ndisor ae 
-        :v SUWUN DAAAAMMMM
-        
-// IKI GARAPANKU 
+\\ Dam iki kok ga kenek, padahal iki titik koma e wis tak deloki ga onok sing kelewat. tapi error e tulisan e "error: expected ';' before ')' token ndek baris 13 sing for i 
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
 int main(){
-	int pemrogramandasar, kalkulus, pti, orkom, nim;
-    char namalengkap[50];
-    float rata = (pemrogramandasar+kalkulus+pti+orkom)/4;
+    int jumlahbil, angka;
 
-    printf("\t\t\t\t\t====Program Cek Kelulusan Nilai Mahasiswa====\n");
-    printf("\t\t\t\t=======Masukkan Nilai Dengan Tepat======\n\n\n");
-    printf("Masukkan Nama Anda: ");
-        scanf("%s", &namalengkap);
-	printf("Masukkan NIM Anda: ");
-		scanf("%d", &nim);
-	printf("\nMasukkan nilai mata kuliah Pemrograman Dasar Anda: ");
-		scanf("%d", &pemrogramandasar);
-	printf("Masukkan nilai mata kuliah Kalkulus Anda: ");
-		scanf("%d", &kalkulus);
-	printf("Masukkan nilai mata kuliah PTI Anda: ");
-		scanf("%d", &pti);
-	printf("Masukkan nilai mata kuliah Organisasi Komputer Anda: ");
-		scanf("%d", &orkom);
-        scanf("%f", &rata);
-	
-	if((pemrogramandasar+kalkulus+pti+orkom)/4 >= 75){
-		printf("Nilai Rata-rata Anda Adalah: ", rata;
-		printf("\nSelamat Anda LULUS");
-	}else{
-		printf("Nilai Rata-rata Anda Adalah: ", rata;
-		printf("\nAnda Belum lulus, dimohon untuk mengulang lagi");	
-	} 
-	
-	return 0;	
+    printf("Masukkan jumlah bilangan: ");
+        scanf("%d", &jumlahbil);
+    float bil[jumlahbil], total, rata;
+    total = 0;
+
+    for(angka=1, angka<=jumlahbil, angka++){
+        printf("Masukkan Bilangan %d: ", angka);
+        scanf("%f", &bil[jumlahbil]);
+        total = total + bil[angka];
+    }
+    rata = total/jumlahbil;
+    printf("Rata-rata dari %d bilangan: %.4f", jumlahbil, rata);
+
+    return 0;
 }
-        
