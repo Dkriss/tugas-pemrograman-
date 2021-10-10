b99a49b9431440e58ad0b18acdca7703cdd4df20
Dkriss <89033170+Dkriss@users.noreply.github.com>
2021-10-10 22:15:26 +0700

    Update Nyoba.c

--- Nyoba.c
+++ Nyoba.c
@@ -1,22 +1,22 @@
-\\ Dam iki kok ga kenek, padahal iki titik koma e wis tak deloki ga onok sing kelewat. tapi error e tulisan e "error: expected ';' before ')' token ndek baris 13 sing for i 
-
 #include <stdio.h>
 
-int main(){
+main(){
     int jumlahbil, angka;
 
-    printf("Masukkan jumlah bilangan: ");
+    printf("Masukkan jumlah nilai: ");
         scanf("%d", &jumlahbil);
     float bil[jumlahbil], total, rata;
     total = 0;
 
-    for(angka=1, angka<=jumlahbil, angka++){
+    for(angka=1; angka<=jumlahbil; angka++){
         printf("Masukkan Bilangan %d: ", angka);
-        scanf("%f", &bil[jumlahbil]);
-        total = total + bil[angka];
+            scanf("%f", &bil[angka]);
+            total = total + bil[jumlahbil];
     }
-    rata = total/jumlahbil;
-    printf("Rata-rata dari %d bilangan: %.4f", jumlahbil, rata);
+    total = total + bil[angka];
+    rata = total / jumlahbil;
+    printf("Total dari jumlah nilai tersebut adalah: %f\n", total);
+    printf("Rata-rata dari %d bilangan: %.2f", jumlahbil, rata);
 
     return 0;
 }
